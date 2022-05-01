61288efc0a149bce3cf0c1ce6800b3c9eac25613
Tuutaans <69729332+Tuutaans@users.noreply.github.com>
2022-05-01 22:02:16 +0545

    info regarding scanner ending appended

--- scans/smtp_usr_enum.py
+++ scans/smtp_usr_enum.py
@@ -6,6 +6,6 @@ def smtp_usr_enum(ip):
         with open('25.txt', 'a') as file:
             file.write('\n\n SMTP-USER-ENUM Output \n\n')
             os.system(f"smtp-user-enum -M VRFY -U /usr/share/seclists/Usernames/Names/names.txt -t {ip}  >> 25.txt")
-            
+            print("Scan Completed....!!!)
     except:
         print("an error occured please check manually wheter the port is open or not or verify the service")
