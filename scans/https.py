91eb059d6c173e60aff590b78f723168cd4264e3
Tuutaans <69729332+Tuutaans@users.noreply.github.com>
2022-05-02 23:02:44 +0545

    wrong value placed

--- scans/https.py
+++ scans/https.py
@@ -2,7 +2,7 @@ import subprocess
 
 def https(ip):
     print("\n[Whatweb]\n")
-    command = f"whatweb -a 1 -v https://{target} -U 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0' \n\n"
+    command = f"whatweb -a 1 -v https://{ip} -U 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/77.0' \n\n"
     coom = subprocess.call(command,shell =True)
     command = f"ffuf -u https://{ip}/FUZZ -w /usr/share/dirb/wordlists/common.txt -e .php,.txt,.html -c -t 100 \n | tee httpsdirs.txt"
     com = subprocess.call(command, shell=True)
